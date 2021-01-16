ad6a33f65dd6ead5a010b07bf20e1192804f1989
onaxiz <onaxiz@tutanota.com>
2021-01-17 01:19:33 +0300

    forms

---
+++ app.py
@@ -0,0 +1,60 @@
+import sqlite3
+from flask import Flask, render_template, g, request
+
+app = Flask(__name__)
+
+
+def connect_db():
+    sql = sqlite3.connect('food_log.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+
+def get_db():
+    if not hasattr(g, 'sqlite3_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqliter_db'):
+        g.sqliter_db.close()
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/view')
+def view():
+    return render_template('day.html')
+
+
+@app.route('/food', methods=['GET', 'POST'])
+def food():
+    db = get_db()
+
+    if request.method == 'POST':
+        name = request.form['food-name']
+        protein = int(request.form['protein'])
+        carbohydrates = int(request.form['carbohydrates'])
+        fat = int(request.form['fat'])
+
+        calories = protein * 4 + carbohydrates * 4 + fat * 9
+
+        db.execute('insert into food (name, protein, carbohydrates, fat, calories) values (?, ?, ?, ?, ?)',
+                   [name, protein, carbohydrates, fat, calories])
+        db.commit()
+
+    cur = db.execute('select name, protein, carbohydrates, fat, calories from food')
+    results = cur.fetchall()
+    # return '<h1>Name: {} Proteint: {} Carbs: {} Fat:{}</h1>'.format(request.form['food-name'], \
+    # request.form['protein'], request.form['carbohydrates'], request.form['fat'])
+
+    return render_template('add_food.html', results=results)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
